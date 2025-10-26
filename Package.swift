28d9a69d5dab36a6eae3c0002828cfbc94ca06d3
savioruz <kheril.dev@gmail.com>
2025-10-27 04:11:51 +0700

    refactor: rename test target to follow naming conventions and remove unused test code

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "OwiTests",
+            name: "owiTests",
             dependencies: ["Owi"]
         ),
     ]
--- Tests/owiTests/owiTests.swift
+++ Tests/owiTests/owiTests.swift
@@ -1,7 +1,1 @@
-import Testing
-
 @testable import Owi
-
-@Test func example() async throws {
-  // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
